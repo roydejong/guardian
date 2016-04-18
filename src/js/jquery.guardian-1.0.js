7584a8f0e60c7d966aeff13836652881b3ffc4e1
Roy de Jong <roy@softwarepunt.nl>
2016-04-18 23:00:07 +0200

    For required text inputs, only reject blank values (do not apply default alnum)

--- src/js/jquery.guardian-1.0.js
+++ src/js/jquery.guardian-1.0.js
@@ -469,15 +469,14 @@ if(!window.console) { var console = { log: function() { } } };
 			val = el.val(),
 			pattern = el.data('pattern');
 			this.$inputs[name] = true;
-			
-			
+
 			if(el.attr('required') || val.length) {
 				if(typeof pattern != 'undefined') {				
 					if(!this.$patterns[pattern].test(val)) {
 						this.$inputs[name] = false;
 					}
 				} else {
-					if(!this.$patterns['alnum'].test(val)) {
+					if(!val.trim().length) {
 						this.$inputs[name] = false;
 					}
 				}
